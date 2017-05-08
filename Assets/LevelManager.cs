2f3d7ed4f9a129b07f3572c35a828f54b9cb17e6
crandino <carlosmanbe@hotmail.com>
2017-05-08 11:29:51 +0200

    First Prototype

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    private GameObject ground;
+    private GameObject turret_marker;
+    private GameObject cannon;
+
+    public GameObject basic_enemy;
+
+    float timer;
+    float time_between_spawns;
+
+    float max_distance_plane = 1.0f;
+
+    bool ground_set = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        turret_marker = GameObject.FindGameObjectWithTag("Pos_Turret");
+        cannon = GameObject.FindGameObjectWithTag("Cannon");
+
+        // Ground generation
+        ground = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        ground.transform.localScale *= max_distance_plane;
+        ground.transform.position = new Vector3(999.9f, 999.9f, 999.9f);
+
+        timer = 0.0f;
+        time_between_spawns = 3.0f;     
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if(!ground_set && cannon.GetComponent<Renderer>().enabled == true)
+        //{
+            ground.transform.position = turret_marker.transform.position;
+        //    ground_set = true;
+        //}           
+
+        if (timer > time_between_spawns)
+        {
+            Vector2 random_pos = Random.insideUnitCircle * max_distance_plane;
+            Vector3 enemy_position = turret_marker.transform.position + new Vector3(random_pos.x, 0.75f, random_pos.y);
+            Instantiate(basic_enemy, enemy_position, Quaternion.identity);
+            timer = 0.0f;
+        }
+        else
+            timer += Time.deltaTime;     
+	}
+}
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour
+{
+    public GameObject ball_prefab;
+    public float power_launch;
+
+    private GameObject plane;
+
+	// Use this for initialization
+	void Start ()
+    { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Track a single touch as a direction control.
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                Vector3 ball_position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1.0f, gameObject.transform.position.z);
+                GameObject ball_instance = (GameObject)Instantiate(ball_prefab, ball_position, Quaternion.identity);
+                ball_instance.GetComponent<Rigidbody>().AddForce(power_launch * gameObject.transform.forward);
+            }
+        }
+    }
+}
