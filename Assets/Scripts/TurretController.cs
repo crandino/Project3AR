2b38ee8eef6259529cd97d255978647d83ade16b
Roger Olasz Fuentes <rogerof1@TR12.CAT>
2017-05-10 11:24:51 +0200

    Solving power charging problems

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class TurretController : MonoBehaviour
 {
     public GameObject ball_prefab;
-    public float power_launch;
+    public float min_power_launch;
+    public float max_power_launch;
 
     private float start_time;
     private float final_time;
@@ -28,6 +29,7 @@ public class TurretController : MonoBehaviour
             //    GameObject ball_instance = (GameObject)Instantiate(ball_prefab, ball_position, Quaternion.identity);
             //    ball_instance.GetComponent<Rigidbody>().AddForce(power_launch * gameObject.transform.forward);
             //}
+            float power_tmp = min_power_launch;
             if(Input.GetTouch(0).phase == TouchPhase.Began)
             {
                 start_time = Time.time * 1000;
@@ -35,23 +37,23 @@ public class TurretController : MonoBehaviour
             if (Input.GetTouch(0).phase == TouchPhase.Ended)
             {
                 final_time = (Time.time * 1000) - start_time;
-                if(final_time > 1500)
+                if(final_time > max_power_launch)
                 {
-                    final_time = 1500;
+                    final_time = max_power_launch;
                 }
-                power_launch += final_time;
+                power_tmp += final_time;
 
                 Vector3 ball_position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1.0f, gameObject.transform.position.z);
                 GameObject ball_instance = (GameObject)Instantiate(ball_prefab, ball_position, Quaternion.identity);
-                ball_instance.GetComponent<Rigidbody>().AddForce(power_launch * gameObject.transform.forward);
+                ball_instance.GetComponent<Rigidbody>().AddForce(power_tmp * gameObject.transform.forward);
             }
         }
         
-        if (Input.anyKeyDown)
-        {
-            Vector3 ball_position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1.0f, gameObject.transform.position.z);
-            GameObject ball_instance = (GameObject)Instantiate(ball_prefab, ball_position, Quaternion.identity);
-            ball_instance.GetComponent<Rigidbody>().AddForce(power_launch * gameObject.transform.forward);
-        }
+        //if (Input.anyKeyDown)
+        //{
+        //    Vector3 ball_position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1.0f, gameObject.transform.position.z);
+        //    GameObject ball_instance = (GameObject)Instantiate(ball_prefab, ball_position, Quaternion.identity);
+        //    ball_instance.GetComponent<Rigidbody>().AddForce(power_launch * gameObject.transform.forward);
+        //}
     }
 }
