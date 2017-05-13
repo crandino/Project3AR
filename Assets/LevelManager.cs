1bacc038f0f09c81d2eda9b4b86fc6eeaee13d0e
crandino <carlosmanbe@hotmail.com>
2017-05-13 10:17:57 +0200

    Added Game Phases

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -3,34 +3,37 @@ using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour
 {
-    private GameObject ground;
+    public enum GAME_PHASES
+    {
+        MENU,
+        GAME
+    }
+
+    private GameObject ground_marker;
     private GameObject turret_marker;
     private GameObject cannon;
 
     // Types of enemies!
     public GameObject enemy_A;
-    public GameObject enemy_B;
-    public GameObject enemy_C;
-    public GameObject enemy_D;
+
+    public bool debug_mode;
 
     float timer;
     float time_between_spawns;
 
-    public List<GameObject> list_of_enemies;
+    [HideInInspector] public List<GameObject> list_of_enemies;
     private List<GameObject> list_of_enemies_to_remove;
+
+    private GAME_PHASES game_phase;
 
 	// Use this for initialization
 	void Start ()
     {
         turret_marker = GameObject.FindGameObjectWithTag("Pos_Turret");
         cannon = GameObject.FindGameObjectWithTag("Cannon");
-        ground = GameObject.FindGameObjectWithTag("Terrain");
+        ground_marker = GameObject.FindGameObjectWithTag("Terrain");
 
-        // Ground generation
-        //ground = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        ////ground.GetComponent<MeshRenderer>().enabled = false;
-        //ground.transform.localScale *= max_distance_plane;
-        //ground.transform.position = new Vector3(999.9f, 999.9f, 999.9f);
+        game_phase = GAME_PHASES.MENU;
 
         timer = 0.0f;
         time_between_spawns = 1.0f;
@@ -42,21 +45,29 @@ public class LevelManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        //if (!ground_set && cannon.GetComponent<Renderer>().enabled == true)
-        //{
-        //    ground.transform.position = turret_marker.transform.position;
-        //    ground_set = true;
-        //}
-
-        UpdateEnemies();
-
-        if (timer > time_between_spawns)
+        switch(game_phase)
         {
-            GenerateEnemy();
-            timer = 0.0f;
-        }
-        else
-            timer += Time.deltaTime;     
+            case (GAME_PHASES.MENU):
+                {
+                    if (AreTargetsReady() || debug_mode )
+                        game_phase = GAME_PHASES.GAME;
+                    break;
+                }
+               
+            case (GAME_PHASES.GAME):
+                {
+                    UpdateEnemies();
+
+                    if (timer > time_between_spawns)
+                    {
+                        GenerateEnemy();
+                        timer = 0.0f;
+                    }
+                    else
+                        timer += Time.deltaTime;
+                }
+                break;
+        }   
 	}
 
     void GenerateEnemy()
@@ -64,13 +75,13 @@ public class LevelManager : MonoBehaviour
         float random_distance = Random.Range(10.0f, 35.0f);
         float random_width = Random.Range(-4.5f, 4.5f);
 
-        Vector3 enemy_position = (ground.transform.position) - (ground.transform.forward * random_distance);
+        Vector3 enemy_position = (ground_marker.transform.position) - (ground_marker.transform.forward * random_distance);
         enemy_position.y = 0.75f;
         enemy_position.x += random_width;
 
         //Vector3 enemy_position = terrain.transform.position + new Vector3(random_pos.x, 0.75f, random_pos.y);
         GameObject enemy = Instantiate(enemy_A, enemy_position, Quaternion.identity) as GameObject;
-        enemy.transform.parent = ground.transform;
+        enemy.transform.parent = ground_marker.transform;
         list_of_enemies.Add(enemy);
     }
 
@@ -93,6 +104,13 @@ public class LevelManager : MonoBehaviour
         {
             if(list_of_enemies.Remove(e)) Destroy(e);
         }       
+    }
+
+    private bool AreTargetsReady()
+    {
+        if (ground_marker.GetComponent<MarkerDetectionScript>().markerDetected() && turret_marker.GetComponent<MarkerDetectionScript>().markerDetected())
+            return true;
+        return false;
     }
 
     void OnTriggerEnter()
