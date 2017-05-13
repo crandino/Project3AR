087466b30a9a5ebfd30559cebe6de9b781e9f24d
RogerOlasz <rogerolasz96@gmail.com>
2017-05-13 12:53:20 +0200

    Untracked files...

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHit : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            Destroy(col.gameObject);         // Ball disappears when hit a cylinder
+        }
+
+        if (col.gameObject.tag == "Enemy_A")
+        {
+
+        }
+
+    }
+}
