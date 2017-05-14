afb2ae8146575e4ec644330ba25806c50de1f1fc
RogerOlasz <rogerolasz96@gmail.com>
2017-05-14 11:37:45 +0200

    Adding Music and Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    AudioSource game_music;
+
+	// Use this for initialization
+	void Start ()
+    {
+        game_music = GameObject.Find("ARCamera").GetComponent<AudioSource>();
+        game_music.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GameObject.FindGameObjectWithTag("GameController").GetComponent<LevelManager>().GetCurrentPhase() == GAME_PHASES.GAME)
+        {
+            game_music.Play();
+        }
+        
+        if (GameObject.FindGameObjectWithTag("GameController").GetComponent<LevelManager>().GetCurrentPhase() == GAME_PHASES.MENU)
+        {
+            game_music.Stop();
+        }
+	
+	}
+}
